17bf321ece41f4b4c49b3ce2e0914d095931b17f
Quest1mc <stefan.quest@gmail.com>
2019-03-27 11:26:41 +0100

    added comments in script js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//create values 
+//get values
+//add values to new array 
+// save array to account
+// create tie class 
+// create instance of tie 
+// add  & store values to instance of tie
+
+// display new tie
+
+
 const name = config-panel-name;
 const materials= config-panel-materials;
 const pattern= config-panel-pattern;
