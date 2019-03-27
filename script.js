e709d02b5fcad0667b8d02ba7714f1847a24eb43
Quest1mc <stefan.quest@gmail.com>
2019-03-27 11:14:49 +0100

    added code to index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+const name = config-panel-name;
+const materials= config-panel-materials;
+const pattern= config-panel-pattern;
+const width = config-panel-width;
+const length = config-panel-length;
+const construction = config-panel-construction;
+const tipping = config-panel-tipping;
+
+
+const configuredTie = [name,]
