20ebf2f39f9cbd19bbd81965df4c16b3064f07d6
Quest1mc <stefan.quest@gmail.com>
2019-04-04 14:47:10 +0200

    added function get values - having some problems with Javascript loading before html requires some research

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-//create values 
+//create values x
 //get values
-//add values to new array 
+//add values to new object xx
 // save array to account
-// create tie class 
-// create instance of tie 
+// create tie class xxx
+// create instance of tie xxx
 // add  & store values to instance of tie
 
 // display new tie
 
 //consider making an object instead
-const configuredTie = [];
+
+
+
+
+//save each tie as a disticnt object
+
+class Tie {
+    constructor(name,material,pattern, width,length,construction,tipping) {
+      this.name = name;
+      this.material = material;
+      this.pattern = pattern;
+      this.width = width;
+      this.length = length;
+      this.construction = construction;
+      this.tipping = tipping;
+
+    }
+  }
+//let favouriteTie = new Tie('favourite','silk','foulard','3.25-inch', '58-inch', 'seven-fold','self-tipped')
+
+
+
+// to do write a function that rechecks the page values once submit has been pressed 
+// this function creates an object from all the values present in the DOM
+
+
+// below is the problem of cannot read value of null for all below names...think about this 
+function getValues(){
+    const name = document.querySelector('.config-panel-name_input').value;
+    const material= document.querySelector('#material').value;
+    const pattern= document.querySelector('#pattern').value;
+    const width = document.querySelector('#width').value;
+    const length = document.querySelector('#length').value;
+    const construction = document.querySelector('#construction').value;
+    const tipping = document.querySelector('#tipping').value;
+    const configuredTie = new Tie(name,material,pattern, width,length,construction,tipping);
+    console.log(configuredTie);
+    return configuredTie
+}
+
+
+
+
+
 const currentTie = {}
 
 
-const name = document.querySelector('.config-panel-name_input').value;
-const materials= document.querySelector('config-panel-materials').value;
-const pattern= document.querySelector('config-panel-pattern').value;
-const width = document.querySelector('config-panel-width').value;
-const length = document.querySelector('config-panel-length').value;
-const construction = document.querySelector('config-panel-construction').value;
-const tipping = document.querySelector('config-panel-tipping').value;
+
+
+
 
 
 //const configuredTie = [name,materials,pattern,width,length,construction,tipping]
